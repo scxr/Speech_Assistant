110fe637bb076463b990b630060abd153461f7ac
cswil <56652842+cswil@users.noreply.github.com>
2019-12-04 16:28:30 +0000

    Create joke_module.py

---
+++ joke_module.py
@@ -0,0 +1,9 @@
+from joke.jokes import *
+def chuck_norris_joke():
+    return chucknorris()
+def dad_joke():
+    return icanhazdad()
+def random_joke():
+    return icndb()
+def geek_joke():
+    return geek()
